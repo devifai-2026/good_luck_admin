5187dbc8890b7c1c0ffa0a71fe2b2f51405cc6a8
sayanpal469 <sayanpal469@gmail.com>
2026-01-08 14:47:05 +0530

    t

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ import Chart from './pages/Chart';
 import ECommerce from './pages/Dashboard/ECommerce';
 import FormElements from './pages/Form/FormElements';
 import FormLayout from './pages/Form/FormLayout';
-import Profile from './pages/Profile';
 import Settings from './pages/Settings';
 import Tables from './pages/Tables';
 import Alerts from './pages/UiElements/Alerts';
--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -2,8 +2,8 @@ import axios, { AxiosInstance } from "axios"
 
 // https://goodluckserver.in/
 const axiosInstance: AxiosInstance = axios.create({
-    baseURL:'http://localhost:8002/good_luck/api/v1'
-    // baseURL:'https://goodluckserver.in/good_luck/api/v1'
+    // baseURL:'http://localhost:8002/good_luck/api/v1'
+    baseURL:'https://goodluckserver.in/good_luck/api/v1'
 })
 
 export default axiosInstance;
